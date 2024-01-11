6a413b16e92916bba44f36d3e6d7443339d0296c
CodingOnBush <mostafa.omrane@proton.me>
2024-01-11 19:21:25 +0100

    code added to open a window but still need to install minilibx

---
+++ inc/fdf.h
@@ -0,0 +1,6 @@
+#ifndef FDF_H
+# define FDF_H
+# include "../lib/ft_printf/include/ft_printf.h"
+# include "../lib/libft/libft.h"
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,5 +2,12 @@ int	main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
+	void	*mlx; //display
+	void	*mlx_win; //window
+	t_data	image; //image
+
+	mlx = mlx_init(); //display init
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!"); //window init
+	image.img = mlx_new_image(mlx, 1920, 1080); //image init
 	return (0);
 }
