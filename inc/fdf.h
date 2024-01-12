eb21004211a4967389b9c6b3bc621cfeac39efdb
Mostafa Omrane <momrane@bess-f2r6s1.clusters.42paris.fr>
2024-01-12 09:57:16 +0100

    save to push on school repo

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 09:27:51 by momrane           #+#    #+#             */
-/*   Updated: 2024/01/12 09:27:53 by momrane          ###   ########.fr       */
+/*   Updated: 2024/01/12 09:56:26 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define FDF_H
 # include "../lib/ft_printf/include/ft_printf.h"
 # include "../lib/libft/libft.h"
+# include <stdlib.h>
 
 #endif
