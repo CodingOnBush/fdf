6df14e960653f0b072bdd836b0164408ecfe21dd
Mostafa Omrane <momrane@bess-f2r2s12.clusters.42paris.fr>
2024-01-17 13:03:49 +0100

    I can use my mouse wheel to zoom in and out but segfault if I zoom out to much

--- src/hook.c
+++ src/hook.c
@@ -6,7 +6,7 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 13:37:37 by momrane           #+#    #+#             */
-/*   Updated: 2024/01/17 12:55:29 by momrane          ###   ########.fr       */
+/*   Updated: 2024/01/17 12:59:35 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,22 @@ static int ft_key_hook(int keycode, t_env *env)
 	return (0);
 }
 
+static int ft_mouse_hook(int button, int x, int y, t_env *env)
+{
+	ft_printf("button: %d\n", button);
+	if (button == 4)
+		env->scale += 10;
+	else if (button == 5)
+		env->scale -= 10;
+	ft_printf("x: %d\n", x);
+	ft_printf("y: %d\n", y);
+	return (0);
+}
+
 void	ft_trigger_hooks(t_env *env)
 {
 	mlx_key_hook(env->win_ptr, ft_key_hook, env);
+	mlx_mouse_hook(env->win_ptr, ft_mouse_hook, env);
 	mlx_hook(env->win_ptr, 2, 1L << 0, ft_escape_hook, env);
 	mlx_hook(env->win_ptr, 17, 1L << 2, ft_cross_hook, env);
 }
